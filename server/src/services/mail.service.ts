5f03fcb6701a2a375a0f17bbabfb2e2f5a5a2baa
NickRowlandson <nicholasrowlandson@gmail.com>
2017-05-28 16:04:29 -0400

    Started work on mail service

--- server/src/services/mail.service.ts
+++ server/src/services/mail.service.ts
@@ -1,11 +1,11 @@
 const nodemailer = require('nodemailer');
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'georgianTest@gmail.com',
-        pass: 'georgianTest1'
-    }
+  service: 'gmail',
+  auth: {
+    user: 'georgianTest@gmail.com',
+    pass: 'georgianTest1'
+  }
 });
 
 class MailService {
@@ -13,19 +13,20 @@ class MailService {
     console.log("SENDING MAIL");
     // setup email data with unicode symbols
     let mailOptions = {
-        from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-        to: 'georgianTest@gmail.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world ?', // plain text body
-        html: '<b>Hello world ?</b>' // html body
+      from: '"Test Ghost 👻" <ghost@test.com>', // sender address
+      to: 'georgianTest@gmail.com', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world ?', // plain text body
+      html: '<b>Hello world ?</b>' // html body
     };
 
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
+      if (error) {
+        return console.log(error);
+      } else {
+        return console.log('Message %s sent: %s', info.messageId, info.response);
+      }
     });
   }
 }
