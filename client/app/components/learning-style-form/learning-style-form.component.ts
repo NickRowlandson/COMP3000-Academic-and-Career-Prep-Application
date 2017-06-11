41fd0b29d89c7a2d6313f452dbee3213e6d86b9c
TimHarasym91 <timharasym@gmail.com>
2017-06-11 19:03:34 -0400

    finalized learning styles form functionality
    title

--- client/app/components/learning-style-form/learning-style-form.component.ts
+++ client/app/components/learning-style-form/learning-style-form.component.ts
@@ -13,6 +13,10 @@ import { AuthService } from '../../services/authentication.service';
 export class LearningStyleComponent {
   @Input() learningStyleForm: LearningStyleForm;
   error: any;
+  totalSeeingPoints: number;
+  totalHearingPoints: number;
+  totalDoingPoints: number;
+
   constructor(private clientService: ClientService, private router: Router, private authService: AuthService) {
     this.learningStyleForm = new LearningStyleForm();
   }
@@ -23,6 +27,42 @@ export class LearningStyleComponent {
             this.router.navigate(['/dashboard']);
         })
         .catch(error => this.error = error); // TODO: Display error message
+  }
+
+  tallyPoints() {
+    var seeingPoints = 0;
+    var hearingPoints = 0;
+    var doingPoints = 0;
+
+    if (this.learningStyleForm.seeing1) { seeingPoints++; }
+    if (this.learningStyleForm.seeing2) { seeingPoints++; }
+    if (this.learningStyleForm.seeing3) { seeingPoints++; }
+    if (this.learningStyleForm.seeing4) { seeingPoints++; }
+    if (this.learningStyleForm.seeing5) { seeingPoints++; }
+    if (this.learningStyleForm.seeing6) { seeingPoints++; }
+    if (this.learningStyleForm.seeing7) { seeingPoints++; }
+    this.totalSeeingPoints = seeingPoints;
+    this.learningStyleForm.seeing = this.totalSeeingPoints;
+
+    if (this.learningStyleForm.hearing1) { hearingPoints++; }
+    if (this.learningStyleForm.hearing2) { hearingPoints++; }
+    if (this.learningStyleForm.hearing3) { hearingPoints++; }
+    if (this.learningStyleForm.hearing4) { hearingPoints++; }
+    if (this.learningStyleForm.hearing5) { hearingPoints++; }
+    if (this.learningStyleForm.hearing6) { hearingPoints++; }
+    if (this.learningStyleForm.hearing7) { hearingPoints++; }
+    this.totalHearingPoints = hearingPoints;
+    this.learningStyleForm.hearing = this.totalHearingPoints;
+
+    if (this.learningStyleForm.doing1) { doingPoints++; }
+    if (this.learningStyleForm.doing2) { doingPoints++; }
+    if (this.learningStyleForm.doing3) { doingPoints++; }
+    if (this.learningStyleForm.doing4) { doingPoints++; }
+    if (this.learningStyleForm.doing5) { doingPoints++; }
+    if (this.learningStyleForm.doing6) { doingPoints++; }
+    if (this.learningStyleForm.doing7) { doingPoints++; }
+    this.totalDoingPoints = doingPoints;
+    this.learningStyleForm.doing = this.totalDoingPoints;
   }
 
   goBack() {
--- client/app/models/learningStyleForm.ts
+++ client/app/models/learningStyleForm.ts
@@ -2,4 +2,25 @@ export class LearningStyleForm {
   seeing: number;
   hearing: number;
   doing: number;
+  seeing1: number;
+  seeing2: number;
+  seeing3: number;
+  seeing4: number;
+  seeing5: number;
+  seeing6: number;
+  seeing7: number;
+  hearing1: number;
+  hearing2: number;
+  hearing3: number;
+  hearing4: number;
+  hearing5: number;
+  hearing6: number;
+  hearing7: number;
+  doing1: number;
+  doing2: number;
+  doing3: number;
+  doing4: number;
+  doing5: number;
+  doing6: number;
+  doing7: number;
 }
