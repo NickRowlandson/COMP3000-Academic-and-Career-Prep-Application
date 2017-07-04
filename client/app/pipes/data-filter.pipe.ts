b011e461ad061bb92430971cd3d372d0c6ed598d
TimHarasym91 <timharasym@gmail.com>
2017-07-03 21:36:23 -0400

    fxed filter again..
    added first and lastname search functionality

--- client/app/pipes/data-filter.pipe.ts
+++ client/app/pipes/data-filter.pipe.ts
@@ -8,7 +8,7 @@ export class DataFilterPipe implements PipeTransform {
 
     transform(array: any[], query: string): any {
         if (query) {
-            return _.filter(array, row => row.firstName.toLowerCase().indexOf(query.toLowerCase()) > -1);
+            return _.filter(array, row => ((row.firstName)+" "+(row.lastName)).toLowerCase().indexOf(query.toLowerCase()) > -1);
         }
         return array;
     }
