5b9ca83898dc1b9729862fab9d7343bcf6c8fc0c
TimHarasym91 <timharasym@gmail.com>
2017-07-18 00:09:22 -0400

    implemented case note insert functionality

--- server/src/routes/StudentRoutes.ts
+++ server/src/routes/StudentRoutes.ts
@@ -19,7 +19,8 @@ class StudentRoutes {
         router.post("/enroll/:_studentID/:_courseID", controller.addToTimetable);
         router.delete("/drop/:_studentID/:_courseID", controller.removeFromTimetable);
         // router.get("/timetable", controller.getTimetables);
-        router.get("/timetable/:_studentID", controller.getTimetablesById);    
+        router.get("/timetable/:_studentID", controller.getTimetablesById);
+        router.post("/caseNotes/:_studentID", controller.createNote);
         return router;
     }
 
